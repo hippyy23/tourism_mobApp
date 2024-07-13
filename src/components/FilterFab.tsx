1a8a02c760f97fefd2b17ce69f059d26b3e6dc51
hippyy23 <hippyy2323@gmail.com>
2024-07-13 17:16:58 +0200

    Categories

--- src/components/FilterFab.tsx
+++ src/components/FilterFab.tsx
@@ -1,91 +1,92 @@
 import { IonFab, IonFabButton, IonIcon, IonFabList } from "@ionic/react";
 import { i18n } from "i18next";
-import churchIconFilter from "../assets/images/art_church.svg"; // Icona chiesa filtro
-import monumentIconFilter from "../assets/images/art_monument.svg"; // Icona monumento filtro
-import museumIconFilter from "../assets/images/art_museum.svg"; // Icona museo filtro
+import naturalValenceIconFilter from "../assets/images/natural_valence.svg"; // Icona chiesa filtro
+import hisCultValenceIconFilter from "../assets/images/his_cult_valence.svg"; // Icona monumento filtro
+import activityIconFilter from "../assets/images/activity.svg"; // Icona museo filtro
 import { layers } from "ionicons/icons";
 
 function FilterFab(props: {
-  i18n: i18n;
-  churchersFilter: boolean;
-  setChurchersFilter: (arg0: boolean) => void;
-  monumentsFilter: boolean;
-  setMonumentsFilter: (arg0: boolean) => void;
-  museumsFilter: boolean;
-  setMuseumsFilter: (arg0: boolean) => void;
-  fabRef: React.RefObject<HTMLIonFabElement>;
+	i18n: i18n;
+	naturalValenceFilter: boolean;
+	setNaturalValenceFilter: (arg0: boolean) => void;
+	hisCultValenceFilter: boolean;
+	setHisCultValenceFilter: (arg0: boolean) => void;
+	activityFilter: boolean;
+	setActivityFilter: (arg0: boolean) => void;
+	fabRef: React.RefObject<HTMLIonFabElement>;
 }) {
-  return (
-    <IonFab
-      vertical="bottom"
-      horizontal="end"
-      className="ion-margin-bottom"
-      ref={props.fabRef}
-    >
-      <IonFabButton>
-        <IonIcon icon={layers} />
-      </IonFabButton>
-      <IonFabList side="top">
-        <IonFabButton
-          class={
-            props.churchersFilter
-              ? "my-ion-fab-button ion-color ion-color-success md fab-button-in-list ion-activatable ion-focusable hydrated"
-              : "my-ion-fab-button-opacity ion-color ion-color-danger md fab-button-in-list ion-activatable ion-focusable hydrated"
-          }
-          onClick={() => {
-            props.setChurchersFilter(!props.churchersFilter);
-          }}
-          data-desc={props.i18n.t("cat_churches")}
-          data-bool={props.churchersFilter}
-        >
-          <IonIcon
-            icon={churchIconFilter}
-            class={
-              props.churchersFilter
-                ? "my-icon md hydrated"
-                : "my-icon-opacity md hydrated"
-            }
-          />
-        </IonFabButton>
-        <IonFabButton
-          class={
-            props.monumentsFilter
-              ? "my-ion-fab-button ion-color ion-color-success md fab-button-in-list ion-activatable ion-focusable hydrated"
-              : "my-ion-fab-button-opacity ion-color ion-color-danger md fab-button-in-list ion-activatable ion-focusable hydrated"
-          }
-          onClick={() => props.setMonumentsFilter(!props.monumentsFilter)}
-          data-desc={props.i18n.t("cat_monuments")}
-        >
-          <IonIcon
-            icon={monumentIconFilter}
-            class={
-              props.monumentsFilter
-                ? "my-icon md hydrated"
-                : "my-icon-opacity md hydrated"
-            }
-          />
-        </IonFabButton>
-        <IonFabButton
-          class={
-            props.museumsFilter
-              ? "my-ion-fab-button ion-color ion-color-success md fab-button-in-list ion-activatable ion-focusable hydrated"
-              : "my-ion-fab-button-opacity ion-color ion-color-danger md fab-button-in-list ion-activatable ion-focusable hydrated"
-          }
-          onClick={() => props.setMuseumsFilter(!props.museumsFilter)}
-          data-desc={props.i18n.t("cat_museums")}
-        >
-          <IonIcon
-            icon={museumIconFilter}
-            class={
-              props.museumsFilter
-                ? "my-icon md hydrated"
-                : "my-icon-opacity md hydrated"
-            }
-          />
-        </IonFabButton>
-      </IonFabList>
-    </IonFab>
-  );
+  	return (
+		<IonFab
+			vertical="bottom"
+			horizontal="end"
+			className="ion-margin-bottom"
+			ref={props.fabRef}
+			activated
+		>
+		<IonFabButton>
+			<IonIcon icon={layers} />
+		</IonFabButton>
+		<IonFabList side="top">
+			<IonFabButton
+			class={
+				props.naturalValenceFilter
+				? "my-ion-fab-button ion-color ion-color-success md fab-button-in-list ion-activatable ion-focusable hydrated"
+				: "my-ion-fab-button-opacity ion-color ion-color-danger md fab-button-in-list ion-activatable ion-focusable hydrated"
+			}
+			onClick={() => {
+				props.setNaturalValenceFilter(!props.naturalValenceFilter);
+			}}
+			data-desc={props.i18n.t("cat_natural_valence")}
+			data-bool={props.naturalValenceFilter}
+			>
+			<IonIcon
+				icon={naturalValenceIconFilter}
+				class={
+				props.naturalValenceFilter
+					? "my-icon md hydrated"
+					: "my-icon-opacity md hydrated"
+				}
+			/>
+			</IonFabButton>
+			<IonFabButton
+				class={
+					props.hisCultValenceFilter
+					? "my-ion-fab-button ion-color ion-color-success md fab-button-in-list ion-activatable ion-focusable hydrated"
+					: "my-ion-fab-button-opacity ion-color ion-color-danger md fab-button-in-list ion-activatable ion-focusable hydrated"
+				}
+				onClick={() => props.setHisCultValenceFilter(!props.hisCultValenceFilter)}
+				data-desc={props.i18n.t("cat_his_cult_valence")}
+			>
+			<IonIcon
+				icon={hisCultValenceIconFilter}
+				class={
+					props.hisCultValenceFilter
+						? "my-icon md hydrated"
+						: "my-icon-opacity md hydrated"
+				}
+			/>
+			</IonFabButton>
+			<IonFabButton
+				class={
+					props.activityFilter
+					? "my-ion-fab-button ion-color ion-color-success md fab-button-in-list ion-activatable ion-focusable hydrated"
+					: "my-ion-fab-button-opacity ion-color ion-color-danger md fab-button-in-list ion-activatable ion-focusable hydrated"
+				}
+				onClick={() => props.setActivityFilter(!props.activityFilter)}
+				data-desc={props.i18n.t("cat_activity")}
+			>
+			<IonIcon
+				icon={activityIconFilter}
+				class={
+					props.activityFilter
+						? "my-icon md hydrated"
+						: "my-icon-opacity md hydrated"
+				}
+			/>
+			</IonFabButton>
+		</IonFabList>
+		</IonFab>
+	);
 }
 
 export default FilterFab;
