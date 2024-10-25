52ad7ad1968118055f3cb9072f7a0c4089e53bef
hippyy23 <hippyy2323@gmail.com>
2024-10-25 09:46:09 +0200

    Tour line

--- src/components/TourOnMap.tsx
+++ src/components/TourOnMap.tsx
@@ -31,10 +31,75 @@ function TourOnMap(props: {
 	});
 
 	/** Coordinate che disegnano l'interesse, vengono invertite di posizione rispetto a quelle ricevute */
-	const polylineTour: [number, number][] =
-		props.tourDetails.geometry.coordinates[0].map(
-			(coordinates: [number, number]) => [coordinates[0], coordinates[1]]
-		);
+	// const polylineTour: [number, number, number][] =
+	// 	props.tourDetails.geometry.coordinates[0].map(
+	// 		(coordinates: [number, number, number]) => [coordinates[0], coordinates[1], coordinates[2]]
+	// 	);
+
+
+	function getColor(elevation: number) {
+		return 	elevation < 800     ?	'#4DD0F7':
+				elevation < 900		?   '#52C6F7':
+				elevation < 1000	?   '#57BBF6':
+				elevation < 1100	?   '#5CB1F6':
+				elevation < 1200	?   '#61A6F5':
+				elevation < 1300	?   '#F7DE4D':
+				elevation < 1400	?   '#F7D352':
+				elevation < 1500	?   '#F6C758':
+				elevation < 1600    ?   '#F6BC5D':
+				elevation < 1700    ?   '#F5B062':
+				elevation < 1800    ?   '#FF5959':
+				elevation < 1900    ?   '#EF5050':
+				elevation < 2000    ?   '#DF4747':
+				elevation < 2000    ?   '#D03F3F':
+				elevation < 2100    ?   '#C03636':
+				elevation < 2200    ?   '#B02D2D':
+										'#B02D2D' ;
+		}
+
+	var currElevation = props.tourDetails.geometry.coordinates[0][0][2];
+	var positions: [number, number, number][] = [];
+	var polylines: JSX.Element[] = [];
+	for (var i = 0; i < props.tourDetails.geometry.coordinates[0].length; i++) {
+		positions.push([props.tourDetails.geometry.coordinates[0][i][0], props.tourDetails.geometry.coordinates[0][i][1], props.tourDetails.geometry.coordinates[0][i][2]]);
+		if (currElevation + 100 < props.tourDetails.geometry.coordinates[0][i][2] || currElevation - 100 > props.tourDetails.geometry.coordinates[0][i][2]) {
+			var color = getColor(currElevation);
+			polylines.push(<Polyline key={i} pathOptions={{ color: color }}  positions={ positions } />);
+			currElevation = props.tourDetails.geometry.coordinates[0][i][2];
+			positions = [];
+		}
+	}
+	if (polylines.length === 0) {
+		color = getColor(positions[0][2]);
+		polylines.push(<Polyline key={i} pathOptions={{ color: color }}  positions={ positions } />);
+	}
+	// props.tourDetails.geometry.coordinates[0].some(
+	// 		(coordinates: [number, number, number]) => {
+	// 			positions.push([coordinates[0], coordinates[1], coordinates[2]]);
+	// 			return currElevation + 100 < coordinates[2];
+	// 		}
+	// 	);
+	// polylines.push(<Polyline key={1} pathOptions={{ color: 'black' }}  positions={ positions } />)
+
+	
+	// var position: [number, number][] = [];
+	// var polylines: [string, typeof position] = ['', []];
+	// var currElevation = props.tourDetails.geometry.coordinates[0][0][2];
+	// console.log(currElevation);
+	// for (let coord: [number, number, number] of props.tourDetails.geometry.coordinates[0]) {
+	// 	if (currElevation > coord[2]) {
+	// 		console.log(">");
+	// 		polylines.concat(getColor(currElevation), position);
+	// 		console.log(polylines);
+	// 		currElevation = coord[2];
+	// 		position.length = 0;
+	// 	} else {
+	// 		console.log("<");
+	// 		position.concat([coord[0], coord[1]]);
+	// 		console.log([coord[0], coord[1]]);
+	// 	}
+	// }
+	// console.log(polylines);
 
 	// Trovare il centro in base ai punti di interesse presenti nel tour
 	// let max = polylineTour.reduce(
@@ -58,29 +123,31 @@ function TourOnMap(props: {
 
 	return (
 		<>
-		<IonFab vertical="top" horizontal="end" className="ion-margin-end">
+		<IonFab vertical="top" horizontal="end" className="ion-margin-end tours">
 			<IonToolbar color="none" className="ion-margin-start ion-padding-start">
-			<IonButtons className="ion-margin-end">
-				{/* Titolo itinerario */}
-				<IonChip
-				class="chip"
-				onClick={() => {
-					setShowTourModal(true);
-				}}
-				>
-				<IonIcon icon={footsteps} color="primary" />
-				<IonLabel>
-					{props.tourDetails.properties[`name_${code}`] ??
-					props.tourDetails.properties.name_en}
-				</IonLabel>
-				</IonChip>
-			</IonButtons>
-			{/* Pulsante per tornare alla mappa originale */}
-			<IonButtons slot="primary">
-				<IonFabButton onClick={() => setCloseTourAlert(true)}>
-				<IonIcon icon={map} />
-				</IonFabButton>
-			</IonButtons>
+				<IonButtons className="ion-margin-end">
+					{/* Titolo itinerario */}
+					<IonChip
+						class="chip"
+						onClick={() => {
+							setShowTourModal(true);
+						}}
+					>
+						<IonIcon icon={ footsteps } color="primary" />
+						<IonLabel>
+							{ 
+								props.tourDetails.properties[`name_${code}`] ??
+								props.tourDetails.properties.name_en 
+							}
+						</IonLabel>
+					</IonChip>
+				</IonButtons>
+				{/* Pulsante per tornare alla mappa originale */}
+				<IonButtons slot="primary">
+					<IonFabButton onClick={ () => setCloseTourAlert(true) }>
+						<IonIcon icon={ map } />
+					</IonFabButton>
+				</IonButtons>
 			</IonToolbar>
 		</IonFab>
 
@@ -90,7 +157,7 @@ function TourOnMap(props: {
 			header={props.i18n.t("tour_alert_title")}
 			message={props.i18n.t("tour_alert_message")}
 			onDidDismiss={() => {
-			setCloseTourAlert(false);
+				setCloseTourAlert(false);
 			}}
 			buttons={[
 			{
@@ -107,18 +174,26 @@ function TourOnMap(props: {
 			]}
 		/>
 
-		<Polyline positions={polylineTour} />
+		{/* <Polyline pathOptions={{color: 'black'}} positions={positions} /> */}
+		{ polylines }
+		{/* <Polyline pathOptions={{color: '#bd0026'}} positions={h800} />
+		<Polyline pathOptions={{color: '#f03b20'}} positions={h1050} /> */}
+		{/* <Polyline pathOptions={{color: '#fd8d3c'}} positions={h1300} /> */}
+		{/* <Polyline pathOptions={{color: '#fecc5c'}} positions={h1550} /> */}
+		{/* <Polyline pathOptions={{color: '#fecc5c'}} positions={h1800} /> */}
+		{/* <Polyline pathOptions={{color: '#fecc5c'}} positions={h2050} /> */}
+		{/* <Polyline pathOptions={{color: '#ffffb2'}} positions={h2300} /> */}
 
 		{showTourModal && (
 			<TourModal
-			openCondition={showTourModal}
-			onDismissConditions={setShowTourModal}
-			data={props.tourDetails}
-			i18n={props.i18n}
-			setTourDetails={props.setTourDetails}
-			closeAllModals={() => {
-				setShowTourModal(false);
-			}}
+				openCondition={showTourModal}
+				onDismissConditions={setShowTourModal}
+				data={props.tourDetails}
+				i18n={props.i18n}
+				setTourDetails={props.setTourDetails}
+				closeAllModals={() => {
+					setShowTourModal(false);
+				}}
 			/>
 		)}
 		</>
