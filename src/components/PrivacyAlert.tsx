5f8f0693c5c4f488d02e6d27ff6b0c5642b042d1
hippyy23 <hippyy2323@gmail.com>
2024-12-04 23:18:18 +0100

    indentation + style whitespace

--- src/components/PrivacyAlert.tsx
+++ src/components/PrivacyAlert.tsx
@@ -3,42 +3,43 @@ import { Preferences } from "@capacitor/preferences";
 import { i18n } from "i18next";
 
 function PrivacyAlert(props: {
-  i18n: i18n;
-  onDismiss: () => void;
-  backdropDismiss: boolean;
+    i18n: i18n;
+    onDismiss: () => void;
+    backdropDismiss: boolean;
 }) {
   return (
-    <>
-      {/* Alert che richiede all'utente se acconsente a farsi tracciare anonimamente */}
-      <IonAlert
-        isOpen={true}
-        header={props.i18n.t("tracking_title")}
-        message={props.i18n.t("tracking_message")}
-        backdropDismiss={props.backdropDismiss}
-        onDidDismiss={props.onDismiss}
-        buttons={[
-          {
-            text: props.i18n.t("agree"),
-            handler: () => {
-              Preferences.set({
-                key: "tracking",
-                value: "y",
-              });
+        <>
+        {/* Alert che richiede all'utente se acconsente a farsi tracciare anonimamente */}
+        <IonAlert
+            isOpen={true}
+            header={props.i18n.t("tracking_title")}
+            style={{whiteSpace: "pre-line"}}
+            message={ props.i18n.t("tracking_message") }
+            backdropDismiss={props.backdropDismiss}
+            onDidDismiss={props.onDismiss}
+            buttons={[
+            {
+                text: props.i18n.t("agree"),
+                handler: () => {
+                Preferences.set({
+                    key: "tracking",
+                    value: "y",
+                });
+                },
             },
-          },
-          {
-            text: props.i18n.t("decline"),
-            handler: () => {
-              Preferences.set({
-                key: "tracking",
-                value: "n",
-              });
+            {
+                text: props.i18n.t("decline"),
+                handler: () => {
+                Preferences.set({
+                    key: "tracking",
+                    value: "n",
+                });
+                },
             },
-          },
-        ]}
-      />
-    </>
-  );
+            ]}
+        />
+        </>
+    );
 }
 
 export default PrivacyAlert;
